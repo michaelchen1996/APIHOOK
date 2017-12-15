cd2044b8bfeaeb9b8065715cb8a160dd8b05bcce
Chen Jiayun <michaelchaen@qq.com>
2017-12-15 20:53:31 +0800

    LogThread: Add Mailslot, SYN status

--- APIHOOK/Monitor/LogThread.cpp
+++ APIHOOK/Monitor/LogThread.cpp
@@ -1,14 +1,15 @@
 #include "stdafx.h"
 #include "LogThread.h"
-#include "Monitor.h"
-
-//HANDLE hSemaphoreInject;
 
 DWORD WINAPI LogThreadProc(LPVOID lpParameter)
 {
 	OutputDebugString(L"This in LogThread\n");
 
 	HANDLE hSemaphoreStatus = NULL;
+	HANDLE hMailslotLog = NULL;
+	DWORD dwMessageSize = 0;
+	DWORD dwMessageRemain = 0;
+	BOOL isStopped = FALSE;
 
 
 	hSemaphoreStatus = CreateSemaphore(NULL, 0, 1, L"APIHOOK_Monitor_Semaphore_Status");
@@ -19,13 +20,34 @@ DWORD WINAPI LogThreadProc(LPVOID lpParameter)
 	}
 
 	//create slot
+	hMailslotLog = CreateMailslot(L"\\\\.\\mailslot\\APIHOOK\\Monitor\\Log", 0, 0, NULL);
+	if (INVALID_HANDLE_VALUE == hMailslotLog)
+	{
+		OutputDebugString(L"CreateMailslot ERROR\n");
+		return 0;
+	}
 
 	ReleaseSemaphore(hSemaphoreInject, 1, NULL);
-
+	//WaitForSingleObject(hSemaphoreStatus, INFINITE);
 	//read from slot and write to file
-	//WaitForSingleObject(hSemaphoreStatus, 0);
+	while (!isStopped || 1 < dwMessageRemain)
+	{
+		if (!isStopped && WAIT_OBJECT_0 == WaitForSingleObject(hSemaphoreStatus, 0))
+		{
+			isStopped = TRUE;
+		}
+		GetMailslotInfo(hMailslotLog, NULL, &dwMessageSize, &dwMessageRemain, NULL);
+		if (0 < dwMessageRemain)
+		{
+			//read
+			//write
+		}
+	}
 	
 	CloseHandle(hSemaphoreStatus);
 	hSemaphoreStatus = NULL;
+
+	OutputDebugString(L"LogThread Finish\n");
+
 	return 0;
 }
